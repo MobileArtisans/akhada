4485431eaf5c9fd99904154d3458de870e37a6f5
Anay Nayak <anayak@thoughtworks.com>
2012-09-10 18:35:00 +0530

    Adding travis-ci, code climate and some gems for faster development

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './akhada'
+
+run Sinatra::Application
---
+++ spec/default_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "42" do
+  it "should be the answer to life, universe and everything" do
+    true.should == true
+  end
+end
